4a6e533ae9a592d777a3488bdf79d6ffdb72489e
David A. Parry <david.parry@igmm.ed.ac.uk>
2016-11-03 11:56:43 +0000

    update in line with vcfhacks

--- VcfReader.pm
+++ VcfReader.pm
@@ -4,7 +4,7 @@ VcfReader.pm - parse VCF headers, lines and search VCFs
 
 =head1 VERSION
 
-version 0.2
+version 0.3
 
 =head1 SYNOPSIS
 
@@ -1041,7 +1041,7 @@ Retrieves the value for a given INFO field from a given line. Returns the value 
 =cut
 sub getVariantInfoField{
     my ($line, $info_field) = @_;
-    my @info = split(';', getVariantField($line, "INFO"));
+    my @info = split(';', $line->[VCF_FIELDS->{INFO}]);
     foreach my $inf (@info){
         if ($inf =~ /^$info_field=(.+)/){
             return $1;
@@ -1259,7 +1259,7 @@ Returns a hash of the different fields found in the FORMAT column for a variant 
 =cut
 sub getVariantFormatFields{
     my ($line) = @_;
-    my $format = getVariantField($line, "FORMAT");
+    my $format = $line->[VCF_FIELDS->{FORMAT}];
     if (not defined $format){
         return;
     }
@@ -1910,7 +1910,7 @@ sub getSampleActualGenotypes{
 
 =item B<getSampleAlleleDepths>
 
-Can be used to return the value for a genotype field (e.g. "GT", "GQ", "AD", "PL") field for one sample, a list of samples or all samples in a VCF.
+Calculates depth for each allele for a sample call using AD or AO/RO genotype fields.
 
 Arguments:
 
@@ -2357,14 +2357,14 @@ sub minimizeAlleles{
     my %min_alleles = ();#key is allele number, each entry is anon hash of CHROM, REF, POS, ALT
     my @al =  readAlleles(line => $line);
     for (my $i = 1; $i < @al; $i++){
-        my ($pos, $ref, $alt) = reduceRefAlt(getVariantField($line, "POS"), $al[0], $al[$i]);
+        my ($pos, $ref, $alt) = reduceRefAlt($line->[VCF_FIELDS->{POS}], $al[0], $al[$i]);
         $min_alleles{$i} = {
-            CHROM           => getVariantField($line, "CHROM"),
+            CHROM           => $line->[VCF_FIELDS->{CHROM}],
             POS             => $pos,
             REF             => $ref,
             ALT             => $alt,
-            ORIGINAL_POS    => getVariantField($line, "POS"),
-            ORIGINAL_REF    => getVariantField($line, "REF"),
+            ORIGINAL_POS    => $line->[VCF_FIELDS->{POS}],
+            ORIGINAL_REF    => $line->[VCF_FIELDS->{REF}],
             ORIGINAL_ALT    => $al[$i],
             ALT_INDEX        => $i,
         };
@@ -2441,7 +2441,7 @@ Requires two array references, one for each split line to compare.
 
 sub variantsHaveMatchingAlleles{
     my ($line1, $line2) = @_;
-    if (getVariantField($line1, "CHROM") ne getVariantField($line2, "CHROM")){
+    if ($line1->[VCF_FIELDS->{CHROM}] ne $line2->[VCF_FIELDS->{CHROM}]){
         return 0;
     }
     my %min1 = minimizeAlleles($line1);
@@ -2622,6 +2622,10 @@ reference to a hash with contig names as keys and their relative order as values
 
 reference to an array of replacement contig ID header lines in the format "##contig=<ID=1,[other fields]>" Use this to replace the contig headers in your VCF in the given order. 
 
+=item tmp_dir
+
+temporary directory to use (only used if Sort::External is installed).
+
 =back
 
  VcfReader::sortVcf(vcf => 'unsorted_file.vcf', output => 'sorted_file.vcf');
@@ -2692,7 +2696,7 @@ sub sortVcf{
         my @sort = ();
         while (my $line = <$FH>){
             next if $line =~ /^#/;
-            my @split = split("\t", $line);
+            my @split = split("\t", $line, 3);
             my $chrom = $split[VCF_FIELDS->{CHROM}];
             my $pos = $split[VCF_FIELDS->{POS}];
             my $s_chrom; 
@@ -2743,10 +2747,11 @@ sub sortVcf{
             $var = $previous;
         }
     }else{
-        my $sortex;
-        if (%contigs){
-            $sortex = Sort::External->new(mem_threshold => 1024**2 * 16);
-        }else{
+        my %sortex_args = (mem_threshold => 1024**2 * 16);
+        if ($args{tmp_dir}){
+            $sortex_args{working_dir} = $args{tmp_dir};
+        }
+        if (not %contigs){
             my $vcfsort = 
                 sub {
                     (my $a_chrom = substr($Sort::External::a, 0, 25)) =~ s/\s+$//;
@@ -2757,16 +2762,16 @@ sub sortVcf{
                     $a_pos cmp $b_pos;
                 };
 
-            $sortex = Sort::External->new(mem_threshold => 1024**2 * 16, 
-                                          sortsub => $vcfsort);
+            $sortex_args{sortsub} = $vcfsort;
         }
+        my $sortex = Sort::External->new(%sortex_args);
         my @feeds = ();
         my $n = 0;
         my $FH = _openFileHandle($args{vcf});
         while (my $line = <$FH>){
             next if ($line =~ /^#/);
             $n++;
-            my @split = split("\t", $line);
+            my @split = split("\t", $line, 3);
             my $chrom = $split[VCF_FIELDS->{CHROM}];
             my $pos = $split[VCF_FIELDS->{POS}];
             my $s_chrom; 
@@ -2881,14 +2886,14 @@ sub sortVariants{
     }else{
         my %temp = ();
         foreach my $l (@$list){
-            $temp{getVariantField($l, 'CHROM')}++;
+            $temp{$l->[VCF_FIELDS->{CHROM}]}++;
         }
         my $n = 0;
         %contigs = map {$_ => $n++} sort byContigs(keys %temp);
     }
     my @sort = sort {
-        $contigs{getVariantField($a, 'CHROM')} <=> $contigs{getVariantField($b, 'CHROM')} ||
-        getVariantField($a, "POS") <=> getVariantField($b, "POS")
+        $contigs{$a->[VCF_FIELDS->{CHROM}]} <=> $contigs{$b->[VCF_FIELDS->{CHROM}]} ||
+        $a->[VCF_FIELDS->{POS}] <=> $b->[VCF_FIELDS->{POS}]
     } @$list;
     return @sort if defined wantarray;
     carp "sortVariants method called in void context ";
@@ -2905,7 +2910,7 @@ Sort method to sort variants by position only (useful if you have an array of va
 
 sub sortByPos{
     my $vars = shift;
-    return sort { getVariantField($a, "POS") <=> getVariantField($b, "POS") } @$vars;
+    return sort { $a->[VCF_FIELDS->{POS}] <=> $b->[VCF_FIELDS->{POS}] } @$vars;
 }
     
 
@@ -3811,7 +3816,10 @@ sub getVepFields{
                 carp "$f feature does not exist in CSQ field ";
                 next;
             }
-            $t_csq{lc$f} = $v[ $args{vep_header} -> {lc($f)} ];
+            $t_csq{lc$f} = defined $v[ $args{vep_header} -> {lc($f)} ] ? 
+                                   $v[ $args{vep_header} -> {lc($f)} ] : 
+                                   '';
+            
         }
         push @return, \%t_csq;
     }
@@ -3974,7 +3982,7 @@ sub readSnpEffHeader{
     carp "Warning - multiple ANN fields found, ignoring all but the most recent field " if @info > 1;
    my $csq_line = $info[-1] ;#assume last applied SnpEff consequences are what we are looking for 
    my @csq_fields = ();    
-   if ($csq_line =~ /Description="Functional annotations: '(.+)'\s*">/){
+   if ($csq_line =~ /Description="Functional annotations: '(.+)' ">/){
        @csq_fields = split(/\s+\|\s+/, $1);
    }else{
        croak "Method 'readSnpEffHeader' couldn't properly read the ANN format from the corresponding INFO line: $csq_line ";
